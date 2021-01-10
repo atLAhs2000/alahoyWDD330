2d29d63fc2d79ee369157c15ab3df5a96991fc7e
MusicalMonarch <alyssalahoy@gmail.com>
2021-01-09 16:17:13 -0800

    adds a js script to add li's to ol for each week

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const links = [
+    {
+        label: "Week 1 Exercise/Notes",
+        url: "week1/index.html"
+    }
+]
+
+for (let i = 0; i < links.length; i++) {
+    let item = document.createElement('li');
+    let itemAnchor = document.createElement('a');
+    
+    itemAnchor.setAttribute('href', links[i].url);
+    itemAnchor.innerHTML = links[i].label;
+    item.appendChild(itemAnchor);
+
+    document.getElementById('worklist').appendChild(item);
+}
