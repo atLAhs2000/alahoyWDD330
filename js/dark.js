3b20b043e34bc6a92de4144233ee1c7017db347a
MusicalMonarch <alyssalahoy@gmail.com>
2021-01-20 14:52:27 -0800

    adds a light mode and some more notes to the week 3 notes

--- js/dark.js
+++ js/dark.js
@@ -1,17 +1,29 @@
 
+//declare vars for date and head element
 var d = new Date();
 var head = document.getElementsByTagName('HEAD')[0];
+
+//create link in document
+let cssLink = document.createElement('link');
+cssLink.rel = 'stylesheet';
+cssLink.type = 'text/css';
 
 //check what time it is
 if (d.getHours() > 16) {
     //check if index html file is in root or in a folder
-    let cssLink = document.createElement('link');
-    cssLink.rel = 'stylesheet';
-    cssLink.type = 'text/css';
     if (document.title == 'Portfolio Directory') {
         cssLink.href = 'css/dark.css';
     } else {
         cssLink.href = '../css/dark.css';
     }
-    head.appendChild(cssLink);
+} else {
+    //check if file is in root or folder
+    if (document.title == 'Portfolio Directory') {
+        cssLink.href = 'css/light.css';
+    } else {
+        cssLink.href = '../css/light.css';
+    }
 }
+
+//append link to the head
+head.appendChild(cssLink);
