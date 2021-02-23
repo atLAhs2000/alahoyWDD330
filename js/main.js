28b9bcb1364aa84d05bb5dd52a4c9e69de0a20df
MusicalMonarch <alyssalahoy@gmail.com>
2021-02-22 18:19:54 -0800

    init commit on week 8 notes

--- js/main.js
+++ js/main.js
@@ -24,8 +24,12 @@ const links = [
         url: "toDo/index.html"
     },
     {
-        label: "Week 7 Exercise/Notes",
+        label: "Week 7 Notes",
         url: "week7/index.html"
+    },
+    {
+        label: "Week 8 Notes",
+        url: "week8/index.html"
     }
 ]
 
