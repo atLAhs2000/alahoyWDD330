a3b8e2e53f91470bc062800d9c29997e7496573d
MusicalMonarch <alyssalahoy@gmail.com>
2021-03-24 18:16:10 -0700

    more changes to message challenge

--- messenger/assets/js/main.js
+++ messenger/assets/js/main.js
@@ -3,7 +3,6 @@ let sendBtn = document.getElementById('sendBtn');
 let updateBtn = document.getElementById('updateBtn');
 var messageScreen = document.getElementById('messages');
 var contact = document.getElementById('contact-name');
-var contactLetter = document.getElementById('contact-letter');
 var numOfContacts = 1;
 
 receiveBtn.addEventListener('click', texting);
@@ -31,8 +30,12 @@ function texting(e) {
 function update() {
     let numContact = document.getElementById('numContacts').value;
     let nameContact = document.getElementById('nameContact').value;
-    //let numContactPlus =  document.createElement('span');
+    let numContactPlus =  document.createElement('span');
     numOfContacts = numContact;
+    if (numOfContacts > 1) {
+        numContactPlus.textContent = '';
+        numContactPlus.textContent = `(${numOfContacts - 1})`;
+        document.getElementById('text-header').appendChild(numContactPlus);
+    }
     contact.innerHTML = nameContact;
-    document.getElementById('contact-letter').innerHTML = `<p>${nameContact.charAt(0).toUpperCase()}</p>`;
 }
